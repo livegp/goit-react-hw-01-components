5e947f078ecb1dce0e912cea8fe3b363c1493e71
Oleksandr <live.gp@gmail.com>
2023-06-16 18:37:05 +0300

    App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,20 @@
-import Profile from "components/Profile/Profile";
-import Statistics from "components/Statistics/Statistics";
-import FriendList from "components/FriendList/FriendList";
-import TransactionHistory from "components/TransactionHistory/TransactionHistory";
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import { Container } from './App.styled';
+import user from 'data/user';
+import data from 'data/data';
+import friends from 'data/friends';
+import transactions from 'data/transactions';
 
 export const App = () => {
   return (
-    <>
-      <Profile />
-      <Statistics />
-      <FriendList />
-      <TransactionHistory />
-    </>
+    <Container>
+      <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
   );
 };
