ddaf7a5eaebe0a6aa0ced16b15a6393796426e16
Oleksandr <live.gp@gmail.com>
2023-06-16 18:36:43 +0300

    add styles

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,14 @@
-import { createGlobalStyle } from 'styled-components'
-import { normalize } from 'styled-normalize'
-import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
 ${normalize}
 ${reset}
-`
+*,
+::after,
+::before {
+    box-sizing: border-box;
+}
+
+`;
