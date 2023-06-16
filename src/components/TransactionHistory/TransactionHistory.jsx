8d5e8bb40f2801dfbaa9005ea7bcd15ef7b30178
Oleksandr <live.gp@gmail.com>
2023-06-16 18:38:05 +0300

    TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,39 +1,39 @@
-import transactions from 'data/transactions';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Head, Table } from './TransactionHistory.styled';
 
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const TransactionHistory = props => {
+const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history" items={transactions}>
-      <thead>
+    <Table>
+      <Head>
         <tr>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
-      </thead>
+      </Head>
       <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
       </tbody>
-    </table>
-  )
-}
+    </Table>
+  );
+};
 
 TransactionHistory.propTypes = {
-  id: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  amount: PropTypes.number.isRequired,
-  currency: PropTypes.string.isRequired
-}
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
 
-export default TransactionHistory
+export default TransactionHistory;
---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  margin: 0 auto;
+  color: #3b5998;
+  width: 100%;
+  text-align: center;
+  box-shadow: 0 1px 6px rgb(46 47 66 / 8%), 0 1px 1px rgb(46 47 66 / 16%),
+    0 2px 1px rgb(46 47 66 / 8%);
+
+  th,
+  td {
+    padding: 10px;
+  }
+
+  th:not(:last-child),
+  td:not(:last-child) {
+    border-right: 1px solid #8b9dc3;
+  }
+
+  tbody tr:nth-child(even) {
+    background-color: #f7f7f7;
+  }
+`;
+
+export const Head = styled.thead`
+  background-color: #3b5998;
+  color: #f7f7f7;
+`;
