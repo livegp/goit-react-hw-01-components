d090524d512650089ddd30551a25470cd6480497
Oleksandr <live.gp@gmail.com>
2023-06-16 18:37:17 +0300

    FriendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import FriendListItem from './FriendListItem/FriendListItem'
+import React from 'react';
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem/FriendListItem';
+import { Container } from './FriendList.styled';
 
-function FriendList(props) {
+const FriendList = ({ friends }) => {
   return (
-    <ul class="friend-list">
-      <FriendListItem/>
-    </ul>
-  )
-}
+    <Container>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          status={isOnline}
+        />
+      ))}
+    </Container>
+  );
+};
 
 FriendList.propTypes = {
-  friends: PropTypes.array
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
 
-
-export default FriendList
+export default FriendList;
---
+++ src/components/FriendList/FriendList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  gap: 10px;
+  width: 100%;
+`;
